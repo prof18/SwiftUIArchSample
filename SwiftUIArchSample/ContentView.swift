eeddb794091512a5b2ca21e79b71daf16c0ca1d2
Marco Gomiero <marco.gomiero.93@gmail.com>
2020-09-27 13:12:50 +0200

    Initial Commit

---
+++ SwiftUIArchSample/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  SwiftUIArchSample
+//
+//  Created by Marco Gomiero on 27/09/2020.
+//  Copyright © 2020 Marco Gomiero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
